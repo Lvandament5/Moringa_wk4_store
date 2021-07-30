7e91fb12b80f5ee5fd41210a5905c0accad1fe3a
lvandament5 <lvandament@gmail.com>
2021-07-30 15:11:26 +0300

    Linking buttons to cart, adding delivery option4

--- js/script.js
+++ js/script.js
@@ -18,3 +18,11 @@ function menutoggle(){
     }
 }
 
+function address() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
